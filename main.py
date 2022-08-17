f037254d6e4e07de147dbf87863ec4a7af840de4
pavel <pavel_vilkov17@mail.ru>
2022-08-17 20:50:37 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# тут буду писать решения своих задач к курсу по аналитике данных
+
+import pandas as pd
+import numpy as np
+
+StudentsPerfomance = pd.read_csv('StudentsPerformance.csv')
+# print(StudentsPerfomance.describe())
+# print(StudentsPerfomance.dtypes)
+# print(StudentsPerfomance.groupby('gender').aggregate({'writing score' : 'mean'}))
+
+# print(StudentsPerfomance.size)
+# print(StudentsPerfomance.loc[StudentsPerfomance.gender == 'female'])
+# mean=StudentsPerfomance['writing score'].mean()
+# print(StudentsPerfomance.loc[StudentsPerfomance['writing score']>mean])
+# print(StudentsPerfomance.loc[StudentsPerfomance.lunch == 'free/reduced'].describe())
+# print(StudentsPerfomance.describe())
+score_collums=[i for i in list(StudentsPerfomance) if 'score' in i]
+# print(StudentsPerfomance[score_collums])
+print(StudentsPerfomance.filter(like='score'))
